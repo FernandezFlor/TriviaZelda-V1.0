b4cb325c031af12d17f0c8d84237ef72bd33cf28
Florencia <flomerfer64@gmail.com>
2023-04-08 03:17:50 -0300

    Updates

---
+++ js/datos_preguntas.js
@@ -0,0 +1,102 @@
+let preguntas = [
+  {
+    pregunta:'¿En cuántas líneas temporales se divide la saga a partir de "The Legend of Zelda: Ocarina of Time"?"',
+    opciones:[
+      'Una línea temporal',
+      'Dos líneas temporales',
+      'Tres líneas temporales'
+    ],
+    imagen_src:'img/preguntas/preg1.jpg',
+    respuesta_correcta: 'Tres líneas temporales'
+  },
+  {
+    pregunta:'¿De qué está compuesta la Trifuerza?',
+    opciones:[
+      'Poder, sabiduria y valor',
+      'Fuego, agua y tierra',
+      'Oro, cobre y plata'
+    ],
+    imagen_src:'img/preguntas/preg2.jpg',
+    respuesta_correcta: 'Poder, sabiduria y valor'
+  },
+  {
+    pregunta:'¿En qué año aparece el primer "The Legend of Zelda"?',
+    opciones:[
+      '1980',
+      '1986',
+      '1989'
+    ],
+    imagen_src:'img/preguntas/preg3.jpg',
+    respuesta_correcta: '1986'
+  },
+  {
+    pregunta:'¿Cuántas mascaras utilizables hay en "The Legend of Zelda: Majora\'s Mask"?',
+    opciones:[
+      '32',
+      '24',
+      '50'
+    ],
+    imagen_src:'img/preguntas/preg4.jpg',
+    respuesta_correcta: '24'
+  },
+  {
+    pregunta:'¿A qué raza le pertenece el siguiente emblema?',
+    opciones:[
+      'Gerudo',
+      'Kokiri',
+      'Zora'
+    ],
+    imagen_src:'img/preguntas/preg5.png',
+    respuesta_correcta: 'Kokiri'
+  },  
+  {
+    pregunta:'¿Cuántos fragmentos de la Trifuerza hay que recolectar en "The Legend of Zelda: The Wind Waker"?',
+    opciones:[
+      '6',
+      '7',
+      '8'
+    ],
+    imagen_src:'img/preguntas/preg6.jpg',
+    respuesta_correcta: '8'
+  },  
+  {
+    pregunta:'¿Quién es el siguiente personaje?',
+    opciones:[
+      'Ravio',
+      'Yuga',
+      'Airín'
+    ],
+    imagen_src:'img/preguntas/preg7.png',
+    respuesta_correcta: 'Yuga'
+  },  
+  {
+    pregunta:'¿Quién es el siguiente personaje?',
+    opciones:[
+      'Mikau',
+      'Darmani',
+      'Koume'
+    ],
+    imagen_src:'img/preguntas/preg8.png',
+    respuesta_correcta: 'Mikau'
+  },  
+  {
+    pregunta:'¿Qué moneda se utiliza en el juego?',
+    opciones:[
+      'Euros',
+      'Diamantes',
+      'Rupias'
+    ],
+    imagen_src:'img/preguntas/preg9.png',
+    respuesta_correcta: 'Rupias'
+  },  
+  {
+    pregunta:'¿En que año fue lanzado "The Legend of Zelda: Twilight Princess" para la consola GameCube?',
+    opciones:[
+      '2004',
+      '2006',
+      '2007'
+    ],
+    imagen_src:'img/preguntas/preg10.jpg',
+    respuesta_correcta: '2006'
+  }             
+];
